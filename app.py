8d86f4f1df719558a887f4479896351aee2df46b
chuviethung07 <chuviethung07@gmail.com>
2025-12-10 10:50:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -347,4 +347,4 @@ if st.button("🚀 SOẠN GIÁO ÁN NGAY"):
 
 # --- CHÂN TRANG ---
 st.markdown("---")
-st.markdown("<div style='text-align: center; color: #666;'>© 2025 - ABC - Trường PTDTBT TH  ĐT: ... </div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: #666;'>© 2025 - ABC - Trường PTDTBT TH ...  ĐT: ... </div>", unsafe_allow_html=True)
